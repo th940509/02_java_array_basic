b4331310b3f54482a21000cfd671e73ae9e64e98
user <user@401-10>
2021-03-30 17:47:56 +0900

    03.30 17:47 (학원)

---
+++ 02_java_array_basic/src/step2_01/array/ArrayEx09_내정답.java
@@ -0,0 +1,110 @@
+package step2_01.array;
+import java.util.Arrays;
+//2021/03/20 15:09 ~ 16:22
+import java.util.Scanner;
+
+/*
+ * 
+ * # 영화관 좌석예매
+ * 
+ * 1. 사용자로부터 좌석번호(index)를 입력받아 예매하는 시스템이다.
+ * 2. 예매가 완료되면 해당 좌석 값을 1로 변경한다.
+ * 3. 이미 예매가 완료된 좌석은 재구매할 수 없다.
+ * 4. 한 좌석당 예매 가격은 12000원이다.
+ * 5. 프로그램 종료 후, 해당 영화관의 총 매출액을 출력한다.
+ * 예)
+ * [X] [X] [X] [X] [X] [X] [X]
+ * 
+ * 1. 좌석 예매
+ * 2. 종료
+ * 
+ * (1번 입력)
+ * 
+ * 좌석선택(1~7) : 1
+ * [X] [O] [X] [X] [X] [X] [X]
+ * 
+ * 1. 좌석 예매
+ * 2. 종료
+ * 
+ * (1번 입력)
+ * 
+ * 좌석선택(1~7) : 3
+ * 
+ * [X] [O] [X] [O] [X] [X] [X]
+ * 
+ * 1. 좌석 예매
+ * 2. 종료
+ *
+ * (1번 입력)
+ * 
+ * 좌석선택(1~7) : 3
+ * 이미 예매가 완료 되었습니다.
+ * 
+ * [X] [O] [X] [O] [X] [X] [X]
+ * 
+ * 1. 좌석 예매
+ * 2. 종료
+ * 
+ * (2번 입력)
+ * ----------------------
+ * 매출액 : 24000원
+ * 
+ */
+
+
+public class ArrayEx09_내정답 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		char seat[] = {'X','X','X','X','X','X','X'};
+		int money = 0;
+		System.out.println("1. 좌석예매");
+		System.out.println("2. 종료");
+		
+		
+	
+		
+		
+		for(int i = 0; i < seat.length; i ++) {
+			System.out.println(Arrays.toString(seat));
+			
+			System.out.print("메뉴를 선택하세요: ");
+			int menuChoice = scan.nextInt();
+		
+			if(menuChoice ==1) {
+				System.out.println("좌석을 선택하세요(1-7): ");
+				int seatChoice = scan.nextInt()-1;
+					
+					if(seatChoice < 0 || seatChoice > 6) {
+						System.out.println("좌석을 다시 선택해 주세요");
+						continue;
+					}	
+					
+					if(seat[seatChoice] == 'X') {
+						seat[seatChoice] = 'O';
+						money += 12000;
+					}
+					
+					else if(seat[seatChoice] == 'O') {
+						System.out.println("이미 선택한 좌석입니다.");
+					}
+			}
+			else if(menuChoice ==2) {
+				System.out.println("매출액은: " + money + "원");
+				break;
+			} 
+			
+			else {
+				System.out.println("1-2번 중에 선택해주세요");
+			}
+		
+		}
+				
+		
+		
+		
+		scan.close();
+	}
+	
+}
---
+++ 02_java_array_basic/src/step2_01/array/ArrayEx10_내정답.java
@@ -0,0 +1,98 @@
+package step2_01.array;
+//2021/03/30 16:27 ~ 16:59
+import java.util.Scanner;
+
+/*
+ * # 즉석복권
+ * 
+ * 1. 숫자 7이 연속으로 3번 등장하면, 당첨복권이다.
+ * 2. 메뉴를 선택해서 1번 2번 3번이 당첨인지 확인한다.
+ * 3. 0번은 반복문을 종료한다.
+ * 
+ */
+
+
+public class ArrayEx10_내정답 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] lotto1 = {0, 0, 7, 7, 7, 0, 0, 0};
+		int[] lotto2 = {7, 0, 7, 7, 0, 0, 0, 0};
+		int[] lotto3 = {7, 0, 7, 7, 7, 0, 7, 0};
+		int sel      = 0;
+		
+		while (true) {
+			
+			boolean isWin = false;
+			int count = 0;
+					
+			System.out.println("\n[1]번복권 결과확인");
+			System.out.println("[2]번복권 결과확인");
+			System.out.println("[3]번복권 결과확인");
+			System.out.println("[0]종료");
+			System.out.print("메뉴 선택 : ");
+			sel = scan.nextInt();
+				
+			if(sel ==1) {
+				for(int i=0; i<8; i++) {
+					if(lotto1[i] == 7) {
+						count++;
+						if(count == 3) {
+							isWin = true;
+						}
+					}
+					else if(lotto1[i] != 7) {
+						count = 0;
+					}
+				}
+			}
+				
+			else if(sel ==2) {
+				for(int i=0; i<8; i++) {
+					if(lotto2[i] == 7) {
+						count++;
+						if(count == 3) {
+							isWin = true;
+						}
+					}
+					else if(lotto2[i] != 7) {
+						count = 0;
+					}
+				}
+			}
+			
+			else if(sel ==3) {
+				for(int i=0; i<8; i++) {
+					if(lotto3[i] == 7) {
+						count++;
+						if(count == 3) {
+							isWin = true;
+						}
+					}
+					else if(lotto3[i] != 7) {
+						count = 0;
+					}
+				}
+			}
+			
+			else if(sel ==0) {
+				break;
+			}
+			else {
+				continue;
+			}
+			
+			if(isWin) {
+				System.out.println("당첨");
+			}
+			else {
+				System.out.println("꽝");
+			}
+		}
+		
+		
+		scan.close();
+	}
+}
